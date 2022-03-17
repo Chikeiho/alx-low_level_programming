950e4eb70e9e526e749f02d8b6dd46456f7f4596
Chikeiho <ihosidney@gmail.com>
2022-03-17 19:15:50 +0000

    6 print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - print  a line
+ * @n: numbers of straigh lines
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+int l;
+
+for (l = 1; l <= n; l++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
