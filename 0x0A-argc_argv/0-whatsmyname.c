7b0067bd17c4098c7494e4556805b9beec7759eb
Chikeiho <ihosidney@gmail.com>
2022-04-02 01:16:41 +0000

    t

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Mian
+ * @argc: int type
+ * @argv: char type
+ * Return: printis files name
+ **/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
