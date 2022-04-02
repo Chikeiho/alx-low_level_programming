ae63ef6c1746a69b89553227cb9b98d1d3082bcc
Chikeiho <ihosidney@gmail.com>
2022-04-02 01:18:17 +0000

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Mian
+ * @argc: int type
+ * @argv: char type
+ * Return: printis files name
+ **/
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
