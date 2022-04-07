6f06659efbcc490e6a252e7c43ff6eb89abaefb8
Chikeiho <ihosidney@gmail.com>
2022-04-07 22:45:08 +0000

    more malloc free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif /* _MAIN_H_ */
+#endif /* Iho Chike Sydney */
